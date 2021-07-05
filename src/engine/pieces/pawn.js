cb4d767ddd926c2d3c8f87c7ac9c1a6a43cf9cb9
Kitty <kittygingell@gmail.com>
2021-07-05 15:30:15 +0100

    test 1

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -1,4 +1,7 @@
 import Piece from './piece';
+import Square from "../square";
+import Player from "../player";
+
 
 export default class Pawn extends Piece {
     constructor(player) {
@@ -6,6 +9,23 @@ export default class Pawn extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+    //    let location = []
+        if (this.player === Player.WHITE) {
+            // for (var row = 0; row < 8; row++) {
+            //     for (var col = 0; col < 8; col++) {
+            //         var currentpiece = board.board[row][col]
+            //         var check = currentpiece typeof Pawn
+            //         if (check){
+            //             location.push(Square.at(row + 1, col))}
+            //
+            //     }
+            // }
+
+            return new Array(Square.at(1,0))
+        }
+        else {
+            return new Array(Square.at(6, 7))
+        }
+
     }
 }
