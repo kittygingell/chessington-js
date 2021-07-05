b67ea0743297fe4cc3477bff79f9514a1940f573
Kitty <kittygingell@gmail.com>
2021-07-05 16:44:09 +0100

    Solution to 4, bishop

--- src/engine/pieces/bishop.js
+++ src/engine/pieces/bishop.js
@@ -1,4 +1,5 @@
 import Piece from './piece';
+import Square from "../square";
 
 export default class Bishop extends Piece {
     constructor(player) {
@@ -6,6 +7,23 @@ export default class Bishop extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
-    }
+        let location = []
+        var place = board.findPiece(this)
+        const north = 7-place.col
+        const east = 7-place.row
+        const south = 7-north
+        const west = 7-east
+        for (var i = 1; i < Math.min(north,east)+1; i++) {
+                location.push(Square.at(place.row + i, place.col + i))
+            }
+        for (var j = 1; j < Math.min(north,west)+1; j++) {
+            location.push(Square.at(place.row - j, place.col + j))
+        }
+        for (var k = 1; k < Math.min(south,east)+1; k++) {
+            location.push(Square.at(place.row + k, place.col - k))
+        }
+        for (var l = 1; l < Math.min(south,west)+1; l++) {
+            location.push(Square.at(place.row - l, place.col - l))
+        } return location
+        }
 }
